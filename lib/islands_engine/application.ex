3d44cfc079165910fd0146a59f90b7c12a444cea
Tyler Godfrey <tyler.godfrey@reachlocal.com>
2018-08-04 21:10:14 -0500

    chapter 4 in progress

--- lib/islands_engine/application.ex
+++ lib/islands_engine/application.ex
@@ -8,6 +8,7 @@ defmodule IslandsEngine.Application do
   def start(_type, _args) do
     # List all child processes to be supervised
     children = [
+      {Registry, keys: :unique, name: Registry.Game}
       # Starts a worker by calling: IslandsEngine.Worker.start_link(arg)
       # {IslandsEngine.Worker, arg},
     ]
